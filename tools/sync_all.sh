477855427467e26ed69a6f55dffb7e25e0e5c6de
jaberry835 <71137396+jaberry835@users.noreply.github.com>
2026-04-02 21:08:01 +0000

    Update sync_all.sh to adjust repository list and permissions

--- tools/sync_all.sh
+++ tools/sync_all.sh
@@ -24,12 +24,13 @@ repos=(
   "https://github.com/adamruderman/UserAccessCheckerApi main"
   "https://github.com/adamruderman/AgentChatV2 main"
   "https://github.com/adamruderman/Rude-MCPServer main"
-  "https://github.com/adamruderman/UserAccessChecker-Python main"
+  #"https://github.com/adamruderman/UserAccessChecker-Python main"
   #"https://github.com/jaberry835/snapseek main"
   #"https://github.com/microsoft/simplechat main"
-  "https://github.com/adamruderman/WebScrapeAndIndex main"
-  "https://github.com/adamruderman/SecureAPI main"
-  "https://github.com/adamruderman/SecureChatExtension main"
+  #"https://github.com/adamruderman/WebScrapeAndIndex main"
+  #"https://github.com/adamruderman/SecureAPI main"
+  "https://github.com/adamruderman/SecureChatExtension main",
+  "https://github.com/adamruderman/SecureChatExtension agent-framework-cli"
 )
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
