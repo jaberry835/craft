7e0eeb1c401b2ceea8e8ff7c940447c9b506aeb1
Jackb <jaberry@msft>
2026-08-12 01:08:13 +0000

    Add/update VSCode configuration files and sync_all.ps1 with new repositories

--- tools/sync_all.ps1
+++ tools/sync_all.ps1
@@ -4,15 +4,18 @@
 $repos = @(
  
     @{ Url = "https://github.com/adamruderman/AgentChatV2"; Branch = "main" }
+    @{ Url = "https://github.com/adamruderman/AgentChatV2"; Branch = "experiment/response-api" }
     @{ Url = "https://github.com/adamruderman/Rude-MCPServer"; Branch = "main" }   
    
     @{ Url = "https://github.com/adamruderman/SecureAPI"; Branch = "main" } 
     @{ Url = "https://github.com/adamruderman/McpServer"; Branch = "main" }
     @{ Url = "https://github.com/adamruderman/SecureChatExtension"; Branch = "main" }
      
-    @{ Url = "https://github.com/adamruderman/junior-studio"; Branch = "main"}
-    @{ Url = "https://github.com/adamruderman/junior-web"; Branch = "main"}
-      @{ Url = "https://github.com/jaberry835/GenerateRichDocs"; Branch = "main"}
+    @{ Url = "https://github.com/adamruderman/junior-studio"; Branch = "main" }
+    @{ Url = "https://github.com/adamruderman/junior-web"; Branch = "main" }
+    @{ Url = "https://github.com/jaberry835/GenerateRichDocs"; Branch = "main" }
+    #@{ Url = "https://github.com/microsoft/simplechat"; Branch = "main" }
+     @{ Url = "https://github.com/jaberry835/curic-demo"; Branch = "main" }
   
 )
 
